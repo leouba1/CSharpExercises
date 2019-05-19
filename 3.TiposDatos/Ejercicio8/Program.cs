f4e124103f642d7d64550880e74d1530ea540703
Leandro Uba <uba.leandro@gmail.com>
2019-05-19 12:37:32 -0300

    Exercise 8

---
+++ 3.TiposDatos/Ejercicio8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of the sphere: ");
+            var r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nArea of the sphere: {0:N2}", 4*Math.PI*Math.Pow(r,2));
+            Console.WriteLine("Volume of the sphere: {0:N2}", ((float)4/3)*Math.PI*Math.Pow(r,3));
+        }
+    }
+}
