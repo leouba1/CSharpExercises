d4767964230baa16f0f2fdd0a827efa2fe155f8b
leouba1 <uba.leandro@gmail.com>
2019-05-19 13:46:51 -0300

    Ejercicios 10 y 11

---
+++ 3.TiposDatos/Ejercicio10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            var first = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            var second = int.Parse(Console.ReadLine());
+
+            if (first%2 == 0 && second%2 == 0)
+                Console.WriteLine("Both are even");
+            else if (first%2 != 0 && second%2 != 0)
+                Console.WriteLine("Both are odd");
+            else
+                Console.WriteLine("One is even and the other is odd");
+        }
+    }
+}
