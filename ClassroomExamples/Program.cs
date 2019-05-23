1edd7f37b033f3e4eddb215cb7484895a97eed2a
Leandro Uba <uba.leandro@gmail.com>
2019-05-23 19:37:30 -0300

    Added sln

---
+++ ClassroomExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassroomExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
