155fa11a793d76ade14f177f651f1b925261493b
Leandro Uba <uba.leandro@gmail.com>
2019-06-04 19:13:57 -0300

    OddOcurrences and CycleRotation

--- ClassroomExamples/Iterations.cs
+++ ClassroomExamples/Iterations.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace ClassroomExamples
 {
+    // var nums = new int[] {5, 8 ,9 , 140};
+    //         foreach(var num in nums)
+    //         {
+    //             Console.WriteLine("Decimal: {0} - Binario: {1} - BinaryGap: {2} ", num, Convert.ToString(num,2), Iterations.BinaryGap(num));
+    //         }            
     public class Iterations
     {        
         public static int BinaryGap(int number)
--- ClassroomExamples/Program.cs
+++ ClassroomExamples/Program.cs
@@ -6,11 +6,7 @@ namespace ClassroomExamples
     {
         static void Main(string[] args)
         {
-            var nums = new int[] {5, 8 ,9 , 140};
-            foreach(var num in nums)
-            {
-                Console.WriteLine("Decimal: {0} - Binario: {1} - BinaryGap: {2} ", num, Convert.ToString(num,2), Iterations.BinaryGap(num));
-            }            
+            CyclicRotation.Test();
         }
     }
 }
